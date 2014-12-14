34569ca66f5fda635ab99b4fe2550d5ea4ee9ae9
SpaceFox <ujueseo.yeou@gmail.com>
2014-12-14 17:31:36 +0100

    Django 1.6 --> 1.7, inclus la migration de South
    https://docs.djangoproject.com/en/1.7/topics/migrations/#upgrading-from-south

--- zds/settings.py
+++ zds/settings.py
@@ -166,7 +166,6 @@ INSTALLED_APPS = (
 
     'easy_thumbnails',
     'easy_thumbnails.optimize',
-    'south',
     'crispy_forms',
     'haystack',
     'munin',
@@ -190,10 +189,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
-
-SOUTH_MIGRATION_MODULES = {
-    'easy_thumbnails': 'easy_thumbnails.south_migrations',
-}
 
 THUMBNAIL_ALIASES = {
     '': {
