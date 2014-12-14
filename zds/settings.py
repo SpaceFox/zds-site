8f592f1f84a451a643f5e74f5ca1832303f22530
SpaceFox <ujueseo.yeou@gmail.com>
2014-12-14 17:50:38 +0100

    Utilisation de BASE_DIR comme dans les settings standards

--- zds/settings.py
+++ zds/settings.py
@@ -3,6 +3,9 @@
 import os
 from django.utils.translation import gettext_lazy as _
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 # INTERNAL_IPS = ('127.0.0.1',)  # debug toolbar
@@ -17,7 +20,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'base.db',
+        'NAME': os.path.join(BASE_DIR, 'base.db'),
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
